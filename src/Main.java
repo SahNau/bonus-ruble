48ad47d63a12a93c211f2be16405e085f273a4cd
Alexander Naumov <aleksnn00@gmail.com>
2021-03-14 11:01:16 +0500

    edit cod

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int startBalance = 100;
         int payment = 1100;
-        int bonus=0;
+        int bonus = 0;
         if (payment >= 1000) {
             bonus = payment / 100;
         }
